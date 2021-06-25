4754bfc68e0139867ff232817a7428aa77466704
Garett Begnoche <garett@Garetts-MacBook-Pro.local>
2021-06-24 23:22:55 -0400

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-import BitPic from './Pages/BitPic';
-//import PictureBox from './PictureBox/PictureBox';
-//import DropZone from './DropZone/DropZone';
+import BitPic from './Pages/BitPic/BitPic';
+import NaviBar from './Components/NaviBar/NaviBar';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' exact component={Home}/>
-        <Route path='/BitPic' component={BitPic}/>
-      </Switch>
-    </Router>
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        crossorigin="anonymous"
+      />
+      <NaviBar/>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/BitPic' component={BitPic}/>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
